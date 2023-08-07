cefaa11756421d77afd49d81d70b6798f6081d8d
Imweeraya <weerayakosaldilok@gmail.com>
2023-08-07 10:46:28 +0700

    add cart model

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,11 +8,18 @@ import { ProductRepository } from '../model/product.repository';
 })
 export class StoreComponent {
     selectedCategory: string | undefined;
+    productsPerPage = 4;
+    selectedPage = 1;
+
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
     //return this.repository.getProducts();
-    return this.repository.getProducts(this.selectedCategory); //เลือก catagory
+    //return this.repository.getProducts(this.selectedCategory); //เลือก catagory
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -21,6 +28,26 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+      this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize); //ต้อง convert to number ก่อน เพราะค่าที่ได้รับมาจาก html เป็น string เสมอ
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 
 
